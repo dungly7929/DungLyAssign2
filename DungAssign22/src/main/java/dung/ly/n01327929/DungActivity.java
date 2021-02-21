07b2a3ef95ee53fe6c06761ba39fe5d1d2d88a51
dungly7929 <danks76429@gmail.com>
2021-02-20 20:08:29 -0500

    Lab all file Commit

---
+++ DungAssign22/src/main/java/dung/ly/n01327929/DungActivity.java
@@ -0,0 +1,15 @@
+//Name: DUNG LY         ID:N01327929
+package dung.ly.n01327929;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DungActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
